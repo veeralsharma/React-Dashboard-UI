34e5bd13b4b5a2259a821f39bc5505841845719d
veeralsharma <veeralsharma0001@gmail.com>
2021-05-14 20:43:13 +0530

    project structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import './css/App.css';
+import './components/Sidebar'
+import './components/Main'
 
 function App() {
   return (
     <div className="App">
       <div className="sidebar-div">
-
+        <Sidebar />
       </div>
       <div className="main-div">
-
+        <Main />
       </div>
     </div>
   );
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './Hero'
+
+function Main() {
+    return (
+        <div>
+            <Hero />
+            <div className="main-lower">
+
+            </div>
+        </div>
+    )
+}
+
+export default Main
